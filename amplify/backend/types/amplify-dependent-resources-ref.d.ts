76b2cb65b4dc03c604c9e8327807ab875b9453d1
Aaron <aaronperez159@yahoo.com>
2022-06-08 11:15:23 -0600

    final

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "notesapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s349a3e919": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
